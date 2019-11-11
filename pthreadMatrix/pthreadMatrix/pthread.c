bd331b4593af30b6adc12de6e11d1d03411797db
Evan Jackson <evan@Evans-MacBook-Air.local>
2019-11-11 15:23:57 -0600

    comments

--- pthreadMatrix/pthreadMatrix/pthread.c
+++ pthreadMatrix/pthreadMatrix/pthread.c
@@ -1,9 +1,157 @@
-//
-//  pthread.c
-//  pthreadMatrix
-//
-//  Created by Evan Jackson on 11/7/19.
-//  Copyright © 2019 Evan Jackson. All rights reserved.
-//
+//**************************************************************
 
-#include "pthread.h"
+// Assignment #4
+
+// Name: Evan Jackson
+
+// Parallel Programming Date: Date of Submission (11/12/2019)
+
+//***************************************************************
+
+// This goal of the program is to take 2 arrays of length
+// 1,000,000, and create 4 more arrays, also of size 1,000,000
+// holding the sum, difference, product, and quotient of the
+// other 2 arrays respecitvely. This take is done in parallel.
+// with each thread handeling one of the functions.
+
+// TO RUN IN STAMPEDE:
+// mpicc pthreads.exe -o JacksonA4YesThreads.c
+// sbatch pthreadsScript
+// (result will show 5 times for 5 runs)
+
+//*****************************************************************
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include <sys/time.h>
+
+#define LENGTH 1000000
+#define THREADCNT 4
+//define struct with global variables
+typedef struct {
+    int *a;
+    int *b;
+    int *addition;
+    int *subtraction;
+    int *multiplication;
+    int *division;
+} ARRAY;
+pthread_t threads[THREADCNT];// define threads
+ARRAY mil;//million size array
+
+void * addArray(void * arg){
+    //create local array
+    int * addition = (int *) malloc(LENGTH * sizeof(int));
+    int i;
+    int *x, *y;
+    //create its own pointer so that it is not accessing
+    //at the same time
+    x = mil.a;
+    y = mil.b;
+    
+    //add array into local array
+    for(i = 0; i < LENGTH; i++)
+        addition[i] = x[i] + y[i];
+    
+    //set global pointer to array that was just created
+    mil.addition = addition;
+    pthread_exit((void*) 0);
+}
+
+void * subArray(void * arg){
+    int * subtraction = (int *) malloc(LENGTH * sizeof(int));
+    int i;
+    int *x, *y;
+    x = mil.a;
+    y = mil.b;
+    
+    for(i = 0; i < LENGTH; i++)
+        subtraction[i] = x[i] - y[i];
+    
+    mil.subtraction = subtraction;
+    pthread_exit((void*) 0);
+}
+
+void * multpArray(void * arg){
+    int * multiplication = (int *) malloc(LENGTH * sizeof(int));
+    int i;
+    int *x, *y;
+    x = mil.a;
+    y = mil.b;
+    
+    for(i = 0; i < LENGTH; i++)
+        multiplication[i] = x[i] * y[i];
+    
+    mil.multiplication = multiplication;
+    pthread_exit((void*) 0);
+}
+
+void * divArray(void * arg){
+    int * division = (int *) malloc(LENGTH * sizeof(int));
+    int i;
+    int *x, *y;
+    x = mil.a;
+    y = mil.b;
+    
+    for(i = 0; i < LENGTH; i++)
+        division[i] = x[i] / y[i];
+    
+    mil.division = division;
+    pthread_exit((void*) 0);
+}
+
+
+int main(int argc, const char * argv[]) {
+    int i;
+    //create attribute & status
+    pthread_attr_t attr;
+    void * status;
+    
+    struct timeval start, end;
+    
+    mil.a = (int *) malloc(LENGTH * sizeof(int));
+    mil.b = (int *) malloc(LENGTH * sizeof(int));
+    //fill arrays a & b
+    for(i = 0; i < LENGTH; i++){
+        mil.a[i] = 1;
+        mil.b[i] = 2;
+    }
+    //Create and send threads
+    pthread_attr_init(&attr);
+    //arrays set as joinable to that they can be organized later
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    
+    gettimeofday(&start, NULL);
+    
+    //send each thread to their own routine. No arguments are needed
+    //since it is creating a local version and then writing it to
+    //global version.
+    pthread_create(&threads[0], &attr, addArray, (void*) 0);
+    pthread_create(&threads[1], &attr, subArray, (void*) 0);
+    pthread_create(&threads[2], &attr, multpArray, (void*) 0);
+    pthread_create(&threads[3], &attr, divArray, (void*) 0);
+    
+    pthread_attr_destroy(&attr);
+    
+    //wait for processes to all be finished before continuing
+    for(i = 0; i < THREADCNT; i++)
+        pthread_join(threads[i], &status);
+    
+    gettimeofday(&end, NULL);
+    
+    //Testing
+//    for(i = 0; i < 10; i++)
+//        printf("Addition [%d] is %d\n", i, mil.addition[i]);
+//    for(i = 0; i < 10; i++)
+//        printf("Subtraction [%d] is %d\n", i, mil.subtraction[i]);
+//    for(i = 0; i < 10; i++)
+//        printf("Multiplication [%d] is %d\n", i, mil.multiplication[i]);
+//    for(i = 0; i < 10; i++)
+//        printf("Division [%d] is %d\n", i, mil.division[i]);
+    
+    //print timing
+    printf("%ld microseconds\n", ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
+    
+    pthread_exit(NULL);
+}
--- pthreadMatrix/pthreadMatrix/serial.c
+++ pthreadMatrix/pthreadMatrix/serial.c
@@ -1,15 +1,112 @@
-//
-//  main.c
-//  pthreadMatrix
-//
-//  Created by Evan Jackson on 11/7/19.
-//  Copyright © 2019 Evan Jackson. All rights reserved.
-//
+//**************************************************************
 
+// Assignment #4
+
+// Name: Evan Jackson
+
+// Parallel Programming Date: Date of Submission (11/12/2019)
+
+//***************************************************************
+
+// This goal of the program is to take 2 arrays of length
+// 1,000,000, and create 4 more arrays, also of size 1,000,000
+// holding the sum, difference, product, and quotient of the
+// other 2 arrays respecitvely. This take is done in serial.
+
+// TO RUN IN STAMPEDE:
+// mpicc serial.exe -o JacksonA4NoThreads.c
+// sbatch serialScript
+// (result will show 5 times for 5 runs)
+
+//*****************************************************************
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+
+#define LENGTH 1000000
+int *a;
+int *b;
+
+int * addArray(){
+    //create sum array
+    int * addition = (int *) malloc(LENGTH * sizeof(int));
+    int i;
+    
+    for(i = 0; i < LENGTH; i++)
+        addition[i] = a[i] + b[i];
+    
+    return addition;
+}
+
+int * subArray(){
+    //create difference array
+    int * subtraction = (int *) malloc(LENGTH * sizeof(int));
+    int i;
+    
+    for(i = 0; i < LENGTH; i++)
+        subtraction[i] = a[i] - b[i];
+    
+    return subtraction;
+}
+
+int * multpArray(){
+    //create product array
+    int * multiplication = (int *) malloc(LENGTH * sizeof(int));
+    int i;
+    
+    for(i = 0; i < LENGTH; i++)
+        multiplication[i] = a[i] * b[i];
+    
+    return multiplication;
+}
+
+int * divArray(){
+    //create quotient array
+    int * division = (int *) malloc(LENGTH * sizeof(int));
+    int i;
+    
+    for(i = 0; i < LENGTH; i++)
+        division[i] = a[i] / b[i];
+    
+    return division;
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int i;
+    
+    struct timeval start, end;
+    
+    //create 2 arrays of size 1,000,000
+    a = (int *) malloc(LENGTH * sizeof(int));
+    b = (int *) malloc(LENGTH * sizeof(int));
+    //fill arrays a & b
+    for(i = 0; i < LENGTH; i++){
+        a[i] = 1;
+        b[i] = 2;
+    }
+    
+    gettimeofday(&start, NULL);
+    //call each function and set pointer to address
+    int * addition = addArray();
+    int * subtraction = subArray();
+    int * multiplication = multpArray();
+    int * division = divArray();
+    
+    gettimeofday(&end, NULL);
+   
+    //Testing
+//    for(i = 0; i < 10; i++)
+//        printf("Addition [%d] is %d\n", i, addition[i]);
+//    for(i = 0; i < 10; i++)
+//        printf("Subtraction [%d] is %d\n", i, subtraction[i]);
+//    for(i = 0; i < 10; i++)
+//        printf("Multiplication [%d] is %d\n", i, multiplication[i]);
+//    for(i = 0; i < 10; i++)
+//        printf("Division [%d] is %d\n", i, division[i]);
+    
+    //print timing
+    printf("%ld microseconds\n", ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
     return 0;
 }
