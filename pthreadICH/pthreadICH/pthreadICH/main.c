127c32915c8989abfe9ac19e71608142aa720acf
Evan Jackson <evan@Evans-MacBook-Air.local>
2019-11-18 15:13:51 -0600

    idk

---
+++ pthreadICH/pthreadICH/pthreadICH/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  pthreadICH
+//
+//  Created by Evan Jackson on 11/14/19.
+//  Copyright © 2019 Evan Jackson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
