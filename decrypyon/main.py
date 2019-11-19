91fded9ec61f3fa046ad9ef544ba96b0165e18ee
Evan Jackson <evan@Evans-MacBook-Air.local>
2019-11-19 11:45:43 -0600

    testing sync

--- decrypyon/main.py
+++ decrypyon/main.py
@@ -10,18 +10,24 @@ def addArray(arr):
     
     return summation
 
+def flipArray(arr):
+    arr = numpy.flip(arr)
+    return arr
+
 a = numpy.arange(640)
 summation = numpy.arange(4)
 for n in range(640):
     a[n] = n
 
 #split array into pieces
-a = numpy.array_split(a, 12)
+a = numpy.array_split(a, 64)
 
 pool = mp.Pool(processes=4)
 summation = pool.map(addArray, a)
+flipped = pool.map(flipArray, a)
 arraySum = numpy.sum(summation)
 
 # sum = addArray(a)
 print(f"Sum array is {summation}\n")
-print(f"Summation of the array is {arraySum}\n")
+print(f"Flipped array is {flipped}\n")
+#print(f"Summation of the array is {arraySum}\n")
